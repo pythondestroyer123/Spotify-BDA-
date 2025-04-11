350fb0b0d2f92cd0e15c4cd12fa3170ca76f18c5
Emiliano <emiliano.rascon22@outlook.com>
2025-04-10 21:46:04 -0700

    DB Connection

---
+++ Spotify/src/main/java/mx/itson/spotify/db/ConnectionDB.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.spotify.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author emili
+ */
+
+public class ConnectionDB {
+    
+    private static final String URL = "jdbc:mariadb://localhost:3306/spotify";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+    
+    public Connection getConnection() {
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            if(connection != null) {
+                System.out.println("Conexión exitosa");
+            }
+        }catch(SQLException ex) {
+            System.out.println("Error in connection: " + ex.toString());
+        }
+        return connection;
+    }
+    
+}
+
---
+++ Spotify/src/main/java/mx/itson/spotify/spotify/Spotify.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.spotify.spotify;
+
+/**
+ *
+ * @author emili
+ */
+public class Spotify {
+
+    public static void main(String[] args) {
+        System.out.println("Calando123");
+    }
+}
