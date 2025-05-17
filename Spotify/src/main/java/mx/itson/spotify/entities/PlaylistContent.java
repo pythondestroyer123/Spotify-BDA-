caca11413923c575cb359e825b816d71a7356e62
Emiliano <emiliano.rascon22@outlook.com>
2025-05-16 22:34:58 -0700

    Pues todo

--- Spotify/src/main/java/mx/itson/spotify/entities/PlaylistContent.java
+++ Spotify/src/main/java/mx/itson/spotify/entities/PlaylistContent.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package mx.itson.spotify.entities;
-
+///////
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
